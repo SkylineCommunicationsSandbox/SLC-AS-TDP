ddcb8166557ce137c57bd26a330dc95316d9500c
Tobe Deprez <tobe.deprez@skyline.be>
2023-09-19 16:31:24 +0200

    Blabla

--- SLC-AS-TDP_1/SLC-AS-TDP_1.cs
+++ SLC-AS-TDP_1/SLC-AS-TDP_1.cs
@@ -3,12 +3,13 @@ namespace SLC_AS_TDP_1
 	using System;
 	using System.Collections.Generic;
 	using System.Globalization;
-	using System.Text;
-	using Skyline.DataMiner.Automation;
-	
-	/// <summary>
-	/// Represents a DataMiner Automation script.
-	/// </summary>
+    using System.Text;
+    using Empower.Library.Room0;
+    using Skyline.DataMiner.Automation;
+
+    /// <summary>
+    /// Represents a DataMiner Automation script.
+    /// </summary>
 	public class Script
 	{
 		/// <summary>
@@ -17,7 +18,10 @@ namespace SLC_AS_TDP_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello world!");
-		}
+			engine.GenerateInformation("Hello world! blabla");
+
+			var order = OrderFactory.CreateOrder("Order 1");
+			order.Dispatch("Tobe Deprez");
+        }
 	}
 }
